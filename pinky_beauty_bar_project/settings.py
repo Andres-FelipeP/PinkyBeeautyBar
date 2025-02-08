cd6eef0c2c1c6b1dca0a6b6a2db81e29965e9f0b
AndresFe0 <afprietol@eafit.edu.co>
2025-02-08 01:47:21 -0500

    schema

--- pinky_beauty_bar_project/settings.py
+++ pinky_beauty_bar_project/settings.py
@@ -87,12 +87,10 @@ WSGI_APPLICATION = 'pinky_beauty_bar_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
+
+DATABASES['default']['OPTIONS'] = {'options': '-c search_path=public'}
 
 
 # Password validation
